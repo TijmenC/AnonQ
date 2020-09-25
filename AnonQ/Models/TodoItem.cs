f956fb9acffad44f7468e1e6a9f763d49d682b31
TimonC <58507497+TimonC@users.noreply.github.com>
2020-09-25 10:51:18 +0200

    Added Context + added connecting string

--- AnonQ/Models/TodoItem.cs
+++ AnonQ/Models/TodoItem.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 
 namespace AnonQ.Models
 {
-    public class ToDoItem
+    public class TodoItem
     {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
     }
 }
