63fabb833a5cd7c40bd34b060a49693fdd75dda5
TimonC <58507497+TimonC@users.noreply.github.com>
2020-11-18 12:03:19 +0100

    Added beginning of Jobs

---
+++ AnonQJobs/DeleteQuestionJob.cs
@@ -0,0 +1,32 @@
+﻿using AnonQ.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnonQJobs
+{
+    public class DeleteQuestionJob : IJob
+    {
+        private readonly IServiceProvider _provider;
+        public DeleteQuestionJob(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            using (var scope = _provider.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetService<QuestionContext>();
+                // fetch customers, send email, update DB
+              
+
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
